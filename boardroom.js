0ee3992c32a4902ae936c1a85bf73250667eef29
Capetonian <andrew.lagrange@strategicthinking.co.za>
2013-08-11 22:50:24 +0200

    revised boardroom.js with backbone.js router for room selection.

--- boardroom.js
+++ boardroom.js
@@ -1,55 +1,86 @@
 Boardroom = Backbone.Model.extend({
 	defaults: {
-		name: undefined, 
-		capacity: 8, 
-		office : 'Cape Town', 
-		display_screen: true, 
-		video_conferencing: false, 
+		name: undefined,
+		capacity: 8,
+		office: 'Cape Town',
+		display_screen: true,
+		video_conferencing: false,
 		air_conditioning: true
 	},
-	initialize: function(attributes, options){
+	initialize: function(attributes, options) {
 		console.log('Initialising ' + attributes.name + ' Boardroom');
 	}
 });
 
 BoardroomView = Backbone.View.extend({
-	initialize:function(){
-		
+	initialize: function() {
+
 		console.log("Initialising BoardroomView");
 
 		this.render();
-	}, 
-	render:function(){
+	},
+	render: function() {
 		var template = _.template($("#boardroom_template").html(), this.datamodel());
 
 		console.log(template);
 
 		$(this.el).html(template);
-	}, 
-	datamodel:function(){
-		if(this.model !== undefined)
-		{
+	},
+	datamodel: function() {
+		if (this.model !== undefined) {
 			return this.model.toJSON();
-		}
-		else 
-		{
+		} else {
 			return {};
 		}
 	}
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-	// Models 
 
-	var esperance = new Boardroom({name: "Esperance"});
-	var dunes = new Boardroom({name: "Dunes"});
-	var miami = new Boardroom({name: "Miami"});
-	var batcave = new Boardroom({name: "Bat Cave"});
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			"*actions": "defaultRoute"
+		}
+	});
+	// Initiate the router 
+	var app_router = new AppRouter;
+	app_router.on('route:defaultRoute', function(actions) {
+		
 
-	// Views
+		// Models 
 
-	var esperance_view = new BoardroomView({el: $("#container"), model: esperance});
+		var esperance = new Boardroom({
+			name: "Esperance"
+		});
+		var dunes = new Boardroom({
+			name: "Dunes"
+		});
+		var miami = new Boardroom({
+			name: "Miami"
+		});
+		var batcave = new Boardroom({
+			name: "Bat Cave"
+		});
+
+		if(actions !== null){
+			var room = eval(actions); /* TODO : Remove eval, rather strap into collection within module */
+		}
+		else{
+			var room = esperance;
+		}
+
+		// Views
+
+		var esperance_view = new BoardroomView({
+			el: $("#container"),
+			model: room
+		});
+
+	})
+	// Start Backbone history a necessary step for bookmarkable URL's 
+	Backbone.history.start();
+
+
 
 });
-
